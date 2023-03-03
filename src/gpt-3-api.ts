40365584efbe2f127396df2a8d333af82647d83b
alias-rahil <rahil.kabani.4@gmail.com>
2023-03-04 02:49:47 +0530

    fix: prompts

--- src/gpt-3-api.ts
+++ src/gpt-3-api.ts
@@ -66,7 +66,7 @@ class Gpt3ApI {
 		const suffix = category ? `About ${adjective} ${category} Couple` : `About ${adjective} Couple`;
 		const ending = this.#getRandom(['Happy', 'Surprise', 'Sad', 'Shocking', 'Unexpected']);
 
-		const body = await this.#generateText(`Using the tips below, write a long, lustful, engaging, deep, detailed, descriptive, explicit, and super high-quality erotica ${suffix} with a ${ending} Ending:
+		const body = await this.#generateText(`Using the tips below, write a long, lustful, engaging, deep, detailed, explicit, and super high-quality erotica ${suffix} with a ${ending} Ending:
 		01. Start with an inspiring idea
 		02. Decide a genre and audience
 		03. Create a setting conducive to eroticism
@@ -76,7 +76,7 @@ class Gpt3ApI {
 		07. Build sexual tension through description and dialogue
 		08. Show, don't tell
 		09. Use tension, conflict, suspense, and cliffhangers
-		10. Be descriptive with vivid, sensory language
+		10. Be descriptive with vivid, seductive, sensory language
 		11. Ensure smooth flow, fully developed characters, and appropriate pacing which keeps the erotica moving forward
 		12. Experiment and push boundaries
 		13. Write with the intention of captivating readers and ensuring that the erotica endures the test of time`);
