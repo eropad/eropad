84009b9659353c9a923d77d6c22f9134a7171aff
alias-rahil <rahil.kabani.4@gmail.com>
2023-03-04 00:45:19 +0530

    fix: add oxford comma

--- src/gpt-3-api.ts
+++ src/gpt-3-api.ts
@@ -65,7 +65,7 @@ class Gpt3ApI {
 		const suffix = category ? `About ${adjective} ${category} Couple` : `About ${adjective} Couple`;
 		const ending = this.#getRandom(['Happy', 'Surprise', 'Sad', 'Shocking']);
 
-		const body = await this.#generateText(`Using the tips below, write a long, engaging, deep, detailed, descriptive, explicit and super high quality erotica ${suffix} with a ${ending} Ending:
+		const body = await this.#generateText(`Using the tips below, write a long, engaging, deep, detailed, descriptive, explicit, and super high quality erotica ${suffix} with a ${ending} Ending:
 		1. Decide a genre and audience
 		2. Start with an inspiring idea
 		3. Develop interesting characters
