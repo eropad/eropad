347afb8f50d8f2891b43dead5af52d34127004b4
alias-rahil <rahil.kabani.4@gmail.com>
2023-03-06 21:23:29 +0530

    feat: enhancements

--- src/gpt-3-api.ts
+++ src/gpt-3-api.ts
@@ -35,24 +35,46 @@ class Gpt3ApI {
 	async #generateText(rawInput: string) {
 		const input = rawInput.toLowerCase().trim();
 
-		const {data: {choices: {0: result}}} = await this.#openai.createCompletion({
-			model: 'text-davinci-003',
-			prompt: input,
-			// eslint-disable-next-line @typescript-eslint/naming-convention
-			max_tokens: 4097 - encode(input).length,
-		});
+		try {
+			const {data: {choices: {0: result}}} = await this.#openai.createChatCompletion({
+				model: 'gpt-3.5-turbo',
+				messages: [{role: 'user', content: input}],
+			});
 
-		const text = result.text?.trim();
+			const text = result.message?.content.trim();
 
-		if (result.finish_reason !== 'stop') {
-			throw new Error(`Error finish_reason is ${result.finish_reason!}`);
+			if (result.finish_reason !== 'stop') {
+				throw new Error(`Error finish_reason is ${result.finish_reason!}`);
+			}
+
+			if (!text) {
+				throw new Error('Error generated empty text');
+			}
+
+			return text;
+		} catch (error: unknown) {
+			console.error(error);
+			console.log('Retrying...');
+
+			const {data: {choices: {0: result}}} = await this.#openai.createCompletion({
+				model: 'text-davinci-003',
+				prompt: input,
+				// eslint-disable-next-line @typescript-eslint/naming-convention
+				max_tokens: 4097 - encode(input).length,
+			});
+
+			const text = result.text?.trim();
+
+			if (result.finish_reason !== 'stop') {
+				throw new Error(`Error finish_reason is ${result.finish_reason!}`);
+			}
+
+			if (!text) {
+				throw new Error('Error generated empty text');
+			}
+
+			return text;
 		}
-
-		if (!text) {
-			throw new Error('Error generated empty text');
-		}
-
-		return text;
 	}
 
 	async #generateThumbnail(input: string) {
@@ -71,19 +93,14 @@ class Gpt3ApI {
 		const suffix = category ? `About ${adjective} ${category} Couple` : `About ${adjective} Couple`;
 		const ending = this.#getRandom(['A Satisfying', 'A Surprise', 'A Sad', 'A Shocking', 'An Unexpected']);
 
-		const body = await this.#generateText(`Using the tips below, generate a long/lengthy (~10,000 words), deep, detailed, engaging, explicit, lustful and super high-quality erotica (don't generate the title) ${suffix} with ${ending} ending:
-		Write a strong opening.
-		Determine the genre and target audience.
-		Begin with an inspiring idea.
-		Create a setting that enhances eroticism.
-		Develop interesting characters, including their physical attributes and motivations/intentions.
-		Create a compelling plot.
-		Build sexual tension through descriptive language and dialogue (show, don't tell).
-		Use tension, conflict, suspense, and cliffhangers.
-		Be descriptive using informal, vivid, seductive, sensory language that may include profanity or vulgarity.
-		Ensure smooth flow, fully developed characters, and appropriate pacing that keeps the erotica moving forward.
-		Experiment and push boundaries by exploring taboos, non-consensual and/or consensual themes such as BDSM, Wholesome, Lovely, Domination, Submission, Horror, Transgressive, Interracial, Hentai, Fantasy, Paranormal, Incest, Polygamy, Orgy, Celebrity, infidelity/cheating, and heartbreak.
-		Write with the intention of captivating readers and ensuring that the erotica endures the test of time and always comes out on top.`);
+		const body = await this.#generateText(
+			`Using the tips provided below, write a long, deep, explicit, lustful and engaging erotica of super high-quality with a length of approximately 10,000 words, detailed descriptions, and ${ending} ending. Your erotica should focus on ${suffix}. Do not add a title.\n\n
+		To start, write a captivating opening that immediately hooks the reader. Next, determine the genre and target audience of your erotica. From there, develop an inspiring idea that will capture the reader's attention and maintain their interest throughout the erotica.
+		To enhance the eroticism, create a setting that is conducive to sexual tension and desire. Develop interesting characters with unique physical attributes, motivations, and intentions. Additionally, create a compelling plot with tension, conflict, suspense, and cliffhangers.
+		As you write, build sexual tension through descriptive language and dialogue, using a "show, don't tell" approach. Use informal, vivid, seductive, and sensory language that may include profanity or vulgarity. Ensure a smooth flow, fully developed characters, and appropriate pacing that keeps the erotica moving forward.
+		Experiment and push boundaries by exploring taboos, including consensual and non-consensual themes such as BDSM, Wholesome, Lovely, Domination, Submission, Horror, Transgressive, Interracial, Hentai, Fantasy, Paranormal, Incest, Polygamy, Orgy, Celebrity, infidelity, cheating, and heartbreak.
+		Lastly, write your erotica with the intention of captivating readers and ensuring that it endures the test of time, leaving a lasting impression on your readers.`,
+		);
 
 		const features = await this.#generateText(`Describe The Physical Features Of The People Involved In The Following Love Story ${suffix}:\n\n\n${body}`);
 		const imgData = await this.#generateThumbnail(`Generate An Ultra HD 4K Featured Image For A Love Story ${suffix} Involving:\n\n\n${features}.`);
